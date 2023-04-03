0e8b3359f9a04d757992b89cbe718ff92cc793d8
yumengmeng <whoyumengmeng@163.com>
2023-04-03 23:15:04 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# 《Python数据科学手册》笔记
+
+# 官方源码
+https://github.com/jakevdp/PythonDataScienceHandbook
+
+安装sklearn
+
+conda install -c intel scikit-learn
---
+++ chapter4/myplot.py
@@ -0,0 +1,7 @@
+# ------- file: myplot.py ------
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.linspace(0, 10, 100)
+plt.plot(x, np.sin(x))
+plt.plot(x, np.cos(x))
+plt.show()
